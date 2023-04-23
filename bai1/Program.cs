0b3eb0d258d61d9a87ac1240cd76f2374f246dc5
midu139 <lengocmydungg139@gmail.com>
2023-04-23 09:21:57 +0700

    init

---
+++ bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tuoi = 18;
+            Console.WriteLine("Tuoi cua toi la: {0}", tuoi);
+            //tuoi = 20;
+            Console.WriteLine("Hai nam sau tuoi cua toi la: {0}", tuoi+2);
+            Console.WriteLine("Hai nam sau tuoi cua toi la: {0}", tuoi );
+            //tuoi = tuoi + 20;
+            Console.WriteLine("Hai muoi nam sau tuoi cua toi la: {0}", tuoi+22);
+            Console.WriteLine("Hai muoi nam sau tuoi cua toi la: {0}", tuoi );
+            Console.ReadKey();
+        }
+    }
+}
